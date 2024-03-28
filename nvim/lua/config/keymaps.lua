6d9aba26f41bf0bd674c44c263617d381c5010ae
Anthony <42468660+Anthony1x@users.noreply.github.com>
2024-03-28 23:43:55 +0100

    Redo everything
    As you do

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+local mapkey = require("util.keymapper").mapkey
+
+-- Directory Navigation
+mapkey("<leader>m", "NvimTreeFocus", "n")
+mapkey("<leader>e", "NvimTreeToggle", "n")
+
+-- Window Management
+mapkey("<leader>sv", "vsplit", "n") -- Split Vertically
+mapkey("<leader>sh", "split", "n") -- Split Horizontally
+mapkey("<leader>sm", "MaximizerToggle", "n") -- Toggle Minimise
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+local mapkey = require("util.keymapper").mapkey
+
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      lazy = false,
+      keys = {
+        mapkey("<leader>fk", "Telescope keymaps", "n"),
+        mapkey("<leader>fh", "Telescope help_tags", "n"),
+        mapkey("<leader>ff", "Telescope find_files", "n"),
+        mapkey("<leader>fg", "Telescope live_grep", "n"),
+        mapkey("<leader>fb", "Telescope buffers", "n"),
+      },
+}
