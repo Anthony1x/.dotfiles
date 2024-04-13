a98874228dfad7d40aece5edfc96e77b06990dea
Anthony1x <antonyturtles@gmail.com>
2024-04-13 20:32:43 +0200

    Change pape

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,7 +5,7 @@ xrandr --output DP-0 --mode 2560x1440 --pos 1280x0
 xrandr --output DP-4 --primary --mode 5120x1440 --pos 0x1440
 
 # Set wallpapers
-feh --bg-fill /home/anthony/Pictures/Wallpaper/Desktop/Catppuccin.png
+feh --bg-fill /home/anthony/Pictures/Wallpaper/Desktop/Firewatch.jpg
 
 swww query || swww init &
 
