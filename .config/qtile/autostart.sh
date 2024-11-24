f4b2e5ba9efcba211975915f05f0858af2ca4efa
Anthony1x <antonyturtles@gmail.com>
2024-11-24 15:24:06 +0100

    idk

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,15 +5,7 @@ sleep 1
 # Set monitors (exported by arandr)
 xrandr --output DP-0 --mode 2560x1440 --pos 1280x0
 xrandr --output DP-4 --primary --mode 5120x1440 --pos 0x1440
-xrandr --output HDMI-0 --off
-# xrandr --output DP-0 --mode 2560x1440 --pos 1280x0 --rotate normal \
-# --output DP-1 --off \
-# --output HDMI-0 --off \
-# --output DP-2 --off \
-# --output DP-3 --off \
-# --output DP-4 --primary --mode 5120x1440 --pos 0x1440 --rotate normal \
-# --output DP-5 --off \
-# --output USB-C-0 --off
+# xrandr --output DP-1 --mode 2560x1440 --pos 1280x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-3 --primary --mode 5120x1440 --pos 0x1440 --rotate normal --output None-1 --off
 
 # Set wallpapers
 feh --bg-fill /home/anthony/Pictures/Wallpaper/Desktop/Firewatch.jpg
@@ -24,6 +16,3 @@ dunst &
 redshift &
 picom -b --config $HOME/.config/qtile/scripts/picom.conf &
 sleep 1
-
-swww query || swww init &
-
