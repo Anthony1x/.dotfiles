e1429d49189a0a11e146926c5a0272db5b864346
Anthony1x <antonyturtles@gmail.com>
2024-12-21 17:21:55 +0100

    nvim conf

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,15 +1,49 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt
 
 TABSIZE = 4
 
 -- Tab / Indentation
-vim.o.tabstop = TABSIZE -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = TABSIZE -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = TABSIZE -- Number of spaces inserted when indenting
+opt.tabstop = TABSIZE
+opt.shiftwidth = TABSIZE
+opt.softtabstop = TABSIZE
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+-- Disable animations
+vim.g.snacks_animate = false
 
 vim.api.nvim_set_option_value("colorcolumn", "120", {})
 
-vim.g.snacks_animate = false
+local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+            vim.opt.relativenumber = true
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        if vim.o.nu then
+            vim.opt.relativenumber = false
+            vim.cmd("redraw")
+        end
+    end,
+})
+
+WINBLEND = 0
+
+if vim.g.neovide then
+    vim.g.neovide_window_blurred = true
+    vim.g.neovide_scroll_animation_length = 0.1
+    WINBLEND = 20
+end
+
+vim.opt.winblend = WINBLEND
