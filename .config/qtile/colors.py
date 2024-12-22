39d69561007430be13c1ec937d05bb110f80c9ec
Anthony1x <antonyturtles@gmail.com>
2024-12-22 16:44:45 +0100

    Remove unnecessary colors

--- .config/qtile/colors.py
+++ .config/qtile/colors.py
@@ -1,21 +1,4 @@
-THELINUXCAST = [
-    ["#141b1f", "#141b1f"],  # 0
-    ["#2D363B", "#2D363B"],  # 1
-    ["#D3C6AA", "#D3C6AA"],  # 2
-    ["#E67E80", "#E67E80"],  # 3
-    ["#E69875", "#E69875"],  # 4
-    ["#DBBC7F", "#DBBC7F"],  # 5
-    ["#B2C98F", "#B2C98F"],  # 6
-    ["#88C096", "#88C096"],  # 7
-    ["#A2C8C3", "#A2C8C3"],  # 8
-    ["#D699B6", "#D699B6"],  # 9
-    ["#7A8478", "#7A8478"],  # 10
-    ["#272E32", "#272E32"],  # 11
-    ["#343E44", "#343E44"],  # 12
-    ["#859289", "#859289"],  # 13
-]
-
-CATPPUCCIN = {
+CATPPUCCIN_BASE = {
     "rosewater": "#F5E0DC",
     "flamingo": "#F2CDCD",
     "pink": "#F5C2E7",
@@ -46,52 +29,10 @@ CATPPUCCIN = {
 }
 
 CATPPUCCIN_MOCHA = {
-    'foreground': CATPPUCCIN["text"],
-    'background': CATPPUCCIN["base"],
-    'alt_background': CATPPUCCIN["mantle"],
-    'disabled': CATPPUCCIN['surface0'],
-    'accent': CATPPUCCIN['red'],
-    'highlight': CATPPUCCIN['green'],
-
+    'foreground': CATPPUCCIN_BASE["text"],
+    'background': CATPPUCCIN_BASE["base"],
+    'alt_background': CATPPUCCIN_BASE["mantle"],
+    'disabled': CATPPUCCIN_BASE['surface0'],
+    'accent': CATPPUCCIN_BASE['red'],
+    'highlight': CATPPUCCIN_BASE['green'],
 }
-
-Gruvbox = {
-    'bg':           '#282828',
-    'bg-alt':       '#3c3836',
-    'bg-alt1':      '#504945',
-    'fg':           '#ebdbb2',
-    'dark-red':     '#cc241d',
-    'red':          '#fb4934',
-    'dark-green':   '#98971a',
-    'green':        '#b8bb26',
-    'dark-yellow':  '#d79921',
-    'yellow':       '#fabd2f',
-    'dark-blue':    '#458588',
-    'blue':         '#83a598',
-    'dark-purple':  '#b16286',
-    'purple':       '#d3869b',
-    'dark-aqua':    '#689d6a',
-    'aqua':         '#8ec07c',
-    'dark-gray':    '#928374',
-    'gray':         '#a89984',
-    'shade1': '#1A237E',
-    'shade2': '#283593',
-    'shade3': '#303F9F',
-    'shade4': '#3949AB',
-    'shade5': '#3F51B5',
-    'shade6': '#5C6BC0',
-    'shade7': '#7986CB',
-    'shade8': '#9FA8DA',
-
-    # 'shade1': '#FFEBEB',
-    # 'shade2': '#FFC9C9',
-    # 'shade3': '#FFA5A5',
-    # 'shade4': '#FF7F7F',
-    # 'shade5': '#FF5E5E',
-    # 'shade6': '#FF3D3D',
-    # 'shade7': '#FF1C1C',
-    # 'shade8': '#FF0000',
-
-}
-
-colors = THELINUXCAST
