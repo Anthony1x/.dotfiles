3d911379d96242200addbcd282744d1c2b17acea
Anthony1x <antonyturtles@gmail.com>
2026-02-01 11:48:42 +0100

    Add dap for nvim that is compatible with vscode syntax

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -1,39 +1,101 @@
 return {
     "mfussenegger/nvim-dap",
+    dependencies = {
+        "mason-org/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+    },
+    keys = {
+        {
+            "<leader>db",
+            function()
+                require("dap").toggle_breakpoint()
+            end,
+            desc = "Toggle Breakpoint",
+        },
+        {
+            "<leader>dc",
+            function()
+                require("dap").continue()
+            end,
+            desc = "Continue",
+        },
+        {
+            "<leader>di",
+            function()
+                require("dap").step_into()
+            end,
+            desc = "Step Into",
+        },
+        {
+            "<leader>do",
+            function()
+                require("dap").step_over()
+            end,
+            desc = "Step Over",
+        },
+        {
+            "<leader>dO",
+            function()
+                require("dap").step_out()
+            end,
+            desc = "Step Out",
+        },
+        {
+            "<leader>dr",
+            function()
+                require("dap").repl.open()
+            end,
+            desc = "Open REPL",
+        },
+        {
+            "<leader>du",
+            function()
+                require("dapui").toggle()
+            end,
+            desc = "Toggle DAP UI",
+        },
+    },
     config = function()
         local dap = require("dap")
+        local dapui = require("dapui")
 
-        -- dap.adapters.php = {
-        -- 	type = "executable",
-        -- 	command = "node",
-        -- 	args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" },
+        dapui.setup()
+
+        -- Ensure php-debug-adapter is installed
+        require("mason-nvim-dap").setup({
+            ensure_installed = { "php" },
+            automatic_installation = true,
+        })
+
+        -- Configure the PHP adapter
+        dap.adapters.php = {
+            type = "executable",
+            command = "node",
+            args = { vim.fn.stdpath("data") .. "/mason/packages/php-debug-adapter/extension/out/phpDebug.js" },
+        }
+
+        -- dap.configurations.php = {
+        --     {
+        --         type = "php",
+        --         request = "launch",
+        --         name = "Listen for Xdebug",
+        --         port = 9003,
+        --     },
         -- }
 
-        dap.configurations.php = {
-            {
-                type = "php",
-                request = "launch",
-                name = "Listen for xdebug",
-                port = "9003",
-                log = true,
-                serverSourceRoot = "/srv/app",
-                localSourceRoot = "~/Documents/Dev/Typo3/bauwirtschaft-bw.de/app",
-            },
-            {
-                name = "Launch currently open script",
-                type = "php",
-                request = "launch",
-                program = "${file}",
-                cwd = "${fileDirname}",
-                port = 0,
-                env = {
-                    XDEBUG_MODE = "debug,develop",
-                    XDEBUG_CONFIG = "client_port=${port}",
-                },
-                runtimeArgs = {
-                    "-dxdebug.start_with_request=yes",
-                },
-            },
-        }
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
     end,
 }
