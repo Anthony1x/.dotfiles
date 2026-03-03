f698f21c7d08896ab4028fc37027cd0faa6d6aa7
Anthony <al@aceart.de>
2026-03-03 13:54:29 +0100

    Do not close dapui upon termination

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -76,15 +76,6 @@ return {
             args = { vim.fn.stdpath("data") .. "/mason/packages/php-debug-adapter/extension/out/phpDebug.js" },
         }
 
-        -- dap.configurations.php = {
-        --     {
-        --         type = "php",
-        --         request = "launch",
-        --         name = "Listen for Xdebug",
-        --         port = 9003,
-        --     },
-        -- }
-
         dap.listeners.before.attach.dapui_config = function()
             dapui.open()
         end
@@ -92,10 +83,10 @@ return {
             dapui.open()
         end
         dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
+            -- dapui.close()
         end
         dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
+            -- dapui.close()
         end
     end,
 }
