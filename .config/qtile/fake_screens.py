b208b84446499c8d355c3f4e6408f6f24efb516e
Anthony1x <antonyturtles@gmail.com>
2026-03-21 14:41:15 +0100

    update

--- .config/qtile/fake_screens.py
+++ .config/qtile/fake_screens.py
@@ -1,8 +1,9 @@
 layout_config = [
     {"name": "16:9 middle", "width": 2560, "screens": 3},
-    {"name": "YouTube optimized", "width": 2230, "screens": 3},
+    {"name": "YouTube optimized", "width": 2250, "screens": 3},
     {"name": "18:9 middle", "width": 2880, "screens": 3},
     {"name": "4:3 middle", "width": 1920, "screens": 3},
+    {"name": "1/3 each", "width": 1706, "screens": 3},
     {"name": "21:9 + 11:9", "width": 3440, "screens": 2},
     {"name": "11:9 + 21:9", "width": 1680, "screens": 2},
     {"name": "21:9 middle", "width": 3440, "screens": 3},
